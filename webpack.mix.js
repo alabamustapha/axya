eaf22a431b695252be7dcb9d5ea0ed964725c6ed
tonyfrenzy <tonyfrenzy@gmail.com>
2018-10-30 05:41:50 +0100

    assets organized

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .styles([
+    'public/css/vendor/font-awesome.min.css',
+    'public/css/custom/dashboard.css',
+    'public/css/custom/override.css'
+   ], 'public/css/all.css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
