3aa5d047a6da03af9a89a76e0f593d450156c0ea
tonyfrenzy <tonyfrenzy@gmail.com>
2018-11-10 10:50:16 +0100

    Some UI update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container" style="margin-top:80px;">
+<div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header text-center">{{ __('Register') }}</div>
 
