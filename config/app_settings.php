952971b61348df64ee49dd44e3756745dc7c79c1
tonyfrenzy <tonyfrenzy@gmail.com>
2019-02-02 16:49:57 +0100

    App Settings Updated

--- config/app_settings.php
+++ config/app_settings.php
@@ -10,77 +10,36 @@ return [
             'icon' => 'fa fa-cog', // (optional)
 
             'inputs' => [
-                [
-                    'name' => 'app_name', // unique key for setting
-                    'type' => 'text', // type of input can be text, number, textarea, select, boolean, checkbox etc.
-                    'label' => 'App Name', // label for input
-                    // optional properties
-                    'placeholder' => 'Application Name', // placeholder for input
-                    'class' => 'form-control', // override global input_class
-                    'style' => '', // any inline styles
-                    'rules' => 'required|min:2|max:20', // validation rules for this input
-                    'value' => 'QCode', // any default value
-                    'hint' => 'You can set the app name here' // help block text for input
-                ],
-                [
-                    'name' => 'logo',
-                    'type' => 'image',
-                    'label' => 'Upload logo',
-                    'hint' => 'Must be an image and cropped in desired size',
-                    'rules' => 'image|max:500',
-                    'disk' => 'public', // which disk you want to upload
-                    'path' => 'app', // path on the disk,
-                    'preview_class' => 'thumbnail',
-                    'preview_style' => 'height:40px'
-                ],
-                [
-                    'name' => 'motto', // unique key for setting
-                    'type' => 'text', // type of input can be text, number, textarea, select, boolean, checkbox etc.
-                    'label' => 'App Motto', // label for input
-                    // optional properties
-                    'placeholder' => 'Application Motto', // placeholder for input
-                    'class' => 'form-control', // override global input_class
-                    'style' => '', // any inline styles
-                    'rules' => 'nullable|min:5|max:100', // validation rules for this input
-                    'value' => '...premium health care', // any default value
-                    'hint' => 'You can set the app motto here' // help block text for input
-                ]
-            ]
-        ],
-        'fees' => [
-            'title' => 'Appoinment Fee Settings',
-            'descriptions' => 'Minimum and maximum fees that can be charged hourly.',
-            'icon' => 'fa fa-currency',
 
-            'inputs' => [
+                /* Application Name */
                 [
-                    'name' => 'min_fee',
-                    'type' => 'number',
-                    'step' => '0.01',
-                    'min'  => 1.00,
-                    'label'=> 'Minimum Fee/Hour',
-                    'placeholder' => 'Minimum Allowed Fee/Hour',
-                    'rules' => 'required|between:1.00,1000000.00',
-                    'data_type' => 'numeric',
+                    'name' => 'app_name',                // unique key for setting
+                    'type' => 'text',                    // type of input can be text, number, textarea, select, boolean, checkbox etc.
+                    'data_type' => 'string',             // data type, string, int, boolean
+                    'label' => 'Application Name',               // label for input
+                    // optional properties
+                    'placeholder' => 'Application Name',       // placeholder for input
+                    'class' => 'form-control form-control-sm', // override global input_class
+                    'style' => '',                             // any inline styles
+                    'rules' => 'required|min:2|max:20',        // validation rules for this input
+                    'value' => 'App Name',                             // any default value
+                    'hint' => 'You can set the app name here'  // help block text for input
                 ],
-                [
-                    'name' => 'max_fee',
-                    'type' => 'number',
-                    'step' => '0.01',
-                    'min'  => 1.00,
-                    'label'=> 'Maximum Fee/Hour',
-                    'placeholder' => 'Maximum Allowed Fee/Hour',
-                    'rules' => 'required|between:1.00,1000000.00',
-                    'data_type' => 'numeric',
-                ]
-            ]
-        ],
-        'email' => [
-            'title' => 'Email Settings',
-            'descriptions' => 'How app email will be sent.',
-            'icon' => 'fa fa-envelope',
 
-            'inputs' => [
+                /* Application Motto */
+                [
+                    'name' => 'motto', 
+                    'type' => 'text', 
+                    'data_type' => 'string', 
+                    'label' => 'App Motto', 
+                    // optional properties
+                    'placeholder' => 'Application Motto', 
+                    'style' => '', 
+                    'rules' => 'nullable|min:5|max:100', 
+                    'hint' => 'You can set the app motto here',
+                ],
+
+                /* Email address of sent emails */
                 [
                     'name' => 'from_email',
                     'type' => 'email',
@@ -88,12 +47,122 @@ return [
                     'placeholder' => 'Application from email',
                     'rules' => 'required|email',
                 ],
+
+                /* Name that appear on sent emails */
                 [
                     'name' => 'from_name',
                     'type' => 'text',
                     'label' => 'Email from Name',
                     'placeholder' => 'Email from Name',
+                ],
+
+                /* Application Support Email */
+                [
+                    'type' => 'email',
+                    'name' => 'app_email',
+                    'label' => 'Support Email', 
+                    'placeholder' => 'Support Email',
+                    'class' => 'form-control form-control-sm',
+                    'style' => '', 
+                    'rules' => 'nullable|email',
+                    'hint' => 'You can set the app support email here'
+                ],
+
+                /* Application Support Email */
+                [
+                    'type' => 'number',
+                    'name' => 'app_phone',
+                    'label' => 'Support Phone', 
+                    'placeholder' => 'Support Phone',
+                    'class' => 'form-control form-control-sm',
+                    'style' => '', 
+                    'rules' => 'nullable|string|max:50',
+                    'hint' => 'You can set the app support phone line here'
+                ],
+
+                
+            ]
+        ],
+        'fees' => [
+            'title' => 'Appoinment Fee Settings',
+            'descriptions' => 'Minimum and maximum fees that can be charged hourly.',
+            'icon' => 'fa fa-currency',
+
+            'inputs' => [/* Base Subscription */
+                [
+                    'name' => 'base_subscription', //weekly
+                    'type' => 'number',
+                    'data_type' => 'numeric',
+                    'label'=> 'Base Subscription Fee (Weekly)',
+                    'placeholder' => 'Subscription fee',
+                    'class' => 'form-control form-control-sm', 
+                    'style' => '', 
+                    'rules' => 'required|between:1.00,1000000.00',
+                    'step' => '0.01',
+                    'min'  => 1.00,
+                    'hint' => 'The base subscription amount per week for doctors. Monthly and yearly subscriptions are calculated along with the set discount.'
+                ],
+
+                /* Base Discount */
+                [
+                    'name' => 'base_discount', //weekly
+                    'type' => 'number',
+                    'data_type' => 'numeric',
+                    'label'=> 'Base Discount on Subscriptions (%)',
+                    'placeholder' => '0.01 - 100',
+                    'class' => 'form-control form-control-sm', 
+                    'style' => '', 
+                    'rules' => 'required|between:1.00,100',
+                    'step' => '0.01',
+                    'min'  => 0.01,
+                    'max'  => 100,
+                    'hint' => 'This is the base discount set on subscription fees in the app.'
+                ],
+
+                /* Min Appointment Fee */
+                [
+                    'name' => 'min_fee',
+                    'type' => 'number',
+                    'data_type' => 'numeric',
+                    'label'=> 'Minimum Fee/Hour',
+                    'placeholder' => 'Minimum Allowed Fee/Hour',
+                    'class' => 'form-control form-control-sm', 
+                    'style' => '', 
+                    'rules' => 'required|between:1.00,1000000.00',
+                    'step' => '0.01',
+                    'min'  => 1.00,
+                    'hint' => 'Minimum fee that can be charged hourly.'
+                ],
+
+                /* Max Appointment Fee */
+                [
+                    'name' => 'max_fee',
+                    'type' => 'number',
+                    'data_type' => 'numeric',
+                    'label'=> 'Maximum Fee/Hour',
+                    'placeholder' => 'Maximum Allowed Fee/Hour',
+                    'class' => 'form-control form-control-sm', 
+                    'style' => '', 
+                    'rules' => 'required|between:1.00,1000000.00',
+                    'step' => '0.01',
+                    'min'  => 1.00,
+                    'hint' => 'Maximum fee that can be charged hourly.'
                 ]
+            ]
+        ],
+        'more' => [
+            'title' => 'More Settings',
+            'descriptions' => 'Description of extra app settings.',
+            'icon' => 'fa fa-question-mark',
+
+            'inputs' => [
+                [
+                    'name' => 'extra stuff',
+                    'type' => 'text',
+                    'label' => 'Extra stuff',
+                    'placeholder' => 'extra stuff',
+                    'rules' => '',
+                ],
             ]
         ]
     ],
@@ -102,7 +171,7 @@ return [
     'url' => 'settings',
 
     // Any middleware you want to run on above route
-    'middleware' => [/*'superadmin'*/],
+    'middleware' => ['auth','superadmin'],
 
     // View settings
     'setting_page_view' => 'app_settings::settings_page',
@@ -110,12 +179,12 @@ return [
 
     // Setting section class setting
     'section_class' => 'card mb-3',
-    'section_heading_class' => 'card-header',
+    'section_heading_class' => 'card-header p-3 bg-info text-white font-weight-bold',
     'section_body_class' => 'card-body',
 
     // Input wrapper and group class setting
     'input_wrapper_class' => 'form-group',
-    'input_class' => 'form-control',
+    'input_class' => 'form-control form-control-sm',
     'input_error_class' => 'has-error',
     'input_invalid_class' => 'is-invalid',
     'input_hint_class' => 'form-text text-muted',
---
+++ resources/views/vendor/app_settings/_settings.blade.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+
+            @includeIf(config('app_settings.flash_partial'))
+
+            <form method="post" action="{{ config('app_settings.url') }}" class="form-horizontal mb-3" enctype="multipart/form-data" role="form">
+                {!! csrf_field() !!}
+
+                @if( isset($settingsUI) && count($settingsUI) )
+
+                    @foreach(array_get($settingsUI, 'sections', []) as $section => $fields)
+                        @component('app_settings::section', compact('fields'))
+                            <div class="{{ array_get($fields, 'section_body_class', config('app_settings.section_body_class', 'card-body')) }}">
+                                <div class="row">
+                                    @foreach(array_get($fields, 'inputs', []) as $field)
+                                        @if(!view()->exists('app_settings::fields.' . $field['type']))
+                                            <div style="background-color: #f7ecb5; box-shadow: inset 2px 2px 7px #e0c492; border-radius: 0.3rem; padding: 1rem; margin-bottom: 1rem">
+                                                Defined setting <strong>{{ $field['name'] }}</strong> with
+                                                type <code>{{ $field['type'] }}</code> field is not supported. <br>
+                                                You can create a <code>fields/{{ $field['type'] }}.blade.php</code> to render this input however you want.
+                                            </div>
+                                        @endif
+                                        <div class="col-md-6">
+                                            @includeIf('app_settings::fields.' . $field['type'] )
+                                        </div>
+                                @endforeach
+                                </div>
+                            </div>
+                        @endcomponent
+                    @endforeach
+                @endif
+
+                <div class="row m-b-md">
+                    <div class="col-md-12">
+                        <button class="btn-primary btn">
+                            {{ array_get($settingsUI, 'submit_btn_text', 'Save Settings') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
