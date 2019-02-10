9d552c8c226fe4a7c4696ccc01efea47603d2c1a
tonyfrenzy <tonyfrenzy@gmail.com>
2019-02-10 22:32:30 +0100

    Appointment can't be booked with an inactive doctor.

--- resources/views/appointments/partials/form.blade.php
+++ resources/views/appointments/partials/form.blade.php
@@ -11,170 +11,183 @@
   </div>
    
   <div class="req-form my-3">
-    @if (auth()->user()->hasInconclusiveAppointmentWithSameDoctor($doctor->id))
+    @if ($doctor->isActive())
 
-    <div class="col text-center font-weight-bold">
-      <p class="alert alert-danger">
-        You have <a href="{{route('appointments.index', Auth::user())}}">an inconclusive appointment</a> with {{ $doctor->name }}</strong> and so is not allowed to create a new one at this time. <br>
-        You may <strong>finalize, update or cancel</strong> the inconclusive appointment.
-      </p>
-    </div>
+      @if (auth()->user()->hasInconclusiveAppointmentWithSameDoctor($doctor->id))
 
-    @elseif (auth()->user()->inconclusiveAppointments()->count() > 1) {
-      <!-- Can't have more than two inconclusives at a time -->
-      <p class="alert alert-danger">
-        You cannot book a new appointment at this time because you have <a class="badge badge-warning" href="{{ route('appointments.index', auth()->user()) }}">{{ $count }}inconclusive appointments</a>. <br>
-        You may <strong>finalize, update or cancel</strong> <span class="badge badge-warning">{{ $toRemove }}</span> of your inconclusive appointments to be able to create a new one.
-      </p>
-
-    @else
-
-      <form action="{{ route('appointments.store') }}" method="post">
-        {{ csrf_field() }} 
-        
-        <div class="form-row mb-2">
-          <div class="col-md-6">
-            <label for="appointment_type">Type*</label>
-            <select id="appointment_type" name="type" class="form-default form-control{{ $errors->has('type') ? ' is-invalid' : '' }}" required>
-              <option value="">Appointment Type</option>
-              <option value="Online" {{old('type') == 'Online' ? 'selected':''}}>Online</option>
-              <option value="Home" {{old('type') == 'Home' ? 'selected':''}}>Home</option>
-            </select>
-
-            @if ($errors->has('type'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('type') }}</strong>
-                </span>
-            @endif                    
-          </div>
-
-          <div class="col-md-6">
-            <label for="day">Select Day*</label>
-            <input type="hidden" name="doctor_id" value="{{$doctor->id}}">
-            <input name="day" value="{{old('day')}}" 
-              type="text" id="datepicker"
-              {{-- type="date" id="day"  --}}
-              minlength="10" maxlength="15" min="{{date('Y-m-d')}}"
-              placeholder="{{date('Y-m-d')}}" autocomplete="off" 
-              class="form-default form-control{{ $errors->has('day') ? ' is-invalid' : '' }}" 
-              required>
-
-            @if ($errors->has('day'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('day') }}</strong>
-                </span>
-            @endif   
-          </div>
+        <div class="col text-center font-weight-bold">
+          <p class="alert alert-danger">
+            You have <a href="{{route('appointments.index', Auth::user())}}">an inconclusive appointment</a> with {{ $doctor->name }}</strong> and so is not allowed to create a new one at this time. <br>
+            You may <strong>finalize, update or cancel</strong> the inconclusive appointment.
+          </p>
         </div>
 
+      @elseif (auth()->user()->inconclusiveAppointments()->count() > 1) {
+        <!-- Can't have more than two inconclusives at a time -->
+        <p class="alert alert-danger">
+          You cannot book a new appointment at this time because you have <a class="badge badge-warning" href="{{ route('appointments.index', auth()->user()) }}">{{ $count }}inconclusive appointments</a>. <br>
+          You may <strong>finalize, update or cancel</strong> <span class="badge badge-warning">{{ $toRemove }}</span> of your inconclusive appointments to be able to create a new one.
+        </p>
 
-        <div class="form-row mb-2" id="timepicker">
-          <div class="col-md-6">
-            <div class="input-group form-default">
-              <div class="input-group-prepend">
-                <span class="input-group-text">From</span>
-              </div>
+      @else
 
-              <input id="from" type="text" name="from" minlength="5" maxlength="8" min="00:00" max="23:59" 
-              value="{{old('from')}}" placeholder="HH:MM AM"
-              id="from" class="time start form-control{{ $errors->has('from') ? ' is-invalid' : '' }}" 
-              required>
-
-              @if ($errors->has('from'))
-                  <span class="invalid-feedback" role="alert">
-                      <strong>{{ $errors->first('from') }}</strong>
-                  </span>
-              @endif
-            </div>
-          </div>
+        <form action="{{ route('appointments.store') }}" method="post">
+          {{ csrf_field() }} 
           
-          <div class="col-md-6">
-            <div class="input-group form-default">
-              <div class="input-group-prepend">
-                <span class="input-group-text">To</span>
-              </div>
+          <div class="form-row mb-2">
+            <div class="col-md-6">
+              <label for="appointment_type">Type*</label>
+              <select id="appointment_type" name="type" class="form-default form-control{{ $errors->has('type') ? ' is-invalid' : '' }}" required>
+                <option value="">Appointment Type</option>
+                <option value="Online" {{old('type') == 'Online' ? 'selected':''}}>Online</option>
+                <option value="Home" {{old('type') == 'Home' ? 'selected':''}}>Home</option>
+              </select>
 
-              <input id="to" type="text" name="to" minlength="5" maxlength="8" min="00:00" max="23:59" 
-              value="{{old('to')}}" placeholder="HH:MM AM"
-              id="to" class="time end form-control{{ $errors->has('to') ? ' is-invalid' : '' }}" 
-              required>
-
-              @if ($errors->has('to'))
+              @if ($errors->has('type'))
                   <span class="invalid-feedback" role="alert">
-                      <strong>{{ $errors->first('to') }}</strong>
+                      <strong>{{ $errors->first('type') }}</strong>
                   </span>
               @endif                    
             </div>
+
+            <div class="col-md-6">
+              <label for="day">Select Day*</label>
+              <input type="hidden" name="doctor_id" value="{{$doctor->id}}">
+              <input name="day" value="{{old('day')}}" 
+                type="text" id="datepicker"
+                {{-- type="date" id="day"  --}}
+                minlength="10" maxlength="15" min="{{date('Y-m-d')}}"
+                placeholder="{{date('Y-m-d')}}" autocomplete="off" 
+                class="form-default form-control{{ $errors->has('day') ? ' is-invalid' : '' }}" 
+                required>
+
+              @if ($errors->has('day'))
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $errors->first('day') }}</strong>
+                  </span>
+              @endif   
+            </div>
           </div>
-        </div>
 
-        <div id="deal" class="col-md-8 offset-md-2 mb-2 p-3 table-responsive text-center" style="border-radius:4px;border:1px solid #ccc;font-size: 12px;display: none;">
-          <div class="tf-flex">
-            <span class="border p-1 w-100">
-                <span class="text-muted" style="font-size: 1rem;">Fee:</span> <br>
-                <kbd>$<strong id="cost"></strong></kbd>
-            </span>
-            <span class="border p-1 w-100">
-                <span class="text-muted" style="font-size: 1rem;">Sessions:</span> <br>
-                <kbd><strong id="no_of_sessions"></strong></kbd>
-            </span>
 
-            <span class="border p-1 w-100">
-                <span class="text-muted" style="font-size: 1rem;">Duration:</span> <br>
-                <kbd><strong id="duration"></strong></kbd>
-            </span>
-            <!-- For jQuery Deal Calculation. -->
-            <input id="session" value="{{$doctor->session}}" type="hidden">
-            <input id="rate" value="{{$doctor->rate}}" type="hidden">
+          <div class="form-row mb-2" id="timepicker">
+            <div class="col-md-6">
+              <div class="input-group form-default">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">From</span>
+                </div>
+
+                <input id="from" type="text" name="from" minlength="5" maxlength="8" min="00:00" max="23:59" 
+                value="{{old('from')}}" placeholder="HH:MM AM"
+                id="from" class="time start form-control{{ $errors->has('from') ? ' is-invalid' : '' }}" 
+                required>
+
+                @if ($errors->has('from'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('from') }}</strong>
+                    </span>
+                @endif
+              </div>
+            </div>
+            
+            <div class="col-md-6">
+              <div class="input-group form-default">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">To</span>
+                </div>
+
+                <input id="to" type="text" name="to" minlength="5" maxlength="8" min="00:00" max="23:59" 
+                value="{{old('to')}}" placeholder="HH:MM AM"
+                id="to" class="time end form-control{{ $errors->has('to') ? ' is-invalid' : '' }}" 
+                required>
+
+                @if ($errors->has('to'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('to') }}</strong>
+                    </span>
+                @endif                    
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div class="form-row mb-2">
-          <div class="col-md-12">
-            <label for="description">Description*</label>
-            <textarea name="description" id="description" style="min-height: 120px;max-height: 150px;" placeholder="Describe in details why you are requesting this appointment" class="form-default form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" required>{{old('description')}}</textarea>
+          <div id="deal" class="col-md-8 offset-md-2 mb-2 p-3 table-responsive text-center" style="border-radius:4px;border:1px solid #ccc;font-size: 12px;display: none;">
+            <div class="tf-flex">
+              <span class="border p-1 w-100">
+                  <span class="text-muted" style="font-size: 1rem;">Fee:</span> <br>
+                  <kbd>$<strong id="cost"></strong></kbd>
+              </span>
+              <span class="border p-1 w-100">
+                  <span class="text-muted" style="font-size: 1rem;">Sessions:</span> <br>
+                  <kbd><strong id="no_of_sessions"></strong></kbd>
+              </span>
 
-            @if ($errors->has('description'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('description') }}</strong>
-                </span>
-            @endif
-          </div> 
-        </div>
-
-        <fieldset id="home-visitation" class="p-3 mb-2 border-1" style="border-radius:4px;border:1px solid #ccc;display: none;">
-          <legend class="h5 py-1">Home Visitation</legend>
+              <span class="border p-1 w-100">
+                  <span class="text-muted" style="font-size: 1rem;">Duration:</span> <br>
+                  <kbd><strong id="duration"></strong></kbd>
+              </span>
+              <!-- For jQuery Deal Calculation. -->
+              <input id="session" value="{{$doctor->session}}" type="hidden">
+              <input id="rate" value="{{$doctor->rate}}" type="hidden">
+            </div>
+          </div>
 
           <div class="form-row mb-2">
-            <label for="address">Address*</label>
-            <input type="text" name="address" maxlength="255" value="{{old('address')}}" placeholder="address for home visit" id="address" class="form-default form-control{{ $errors->has('address') ? ' is-invalid' : '' }}">
+            <div class="col-md-12">
+              <label for="description">Description*</label>
+              <textarea name="description" id="description" style="min-height: 120px;max-height: 150px;" placeholder="Describe in details why you are requesting this appointment" class="form-default form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" required>{{old('description')}}</textarea>
 
-            @if ($errors->has('address'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('address') }}</strong>
-                </span>
-            @endif
+              @if ($errors->has('description'))
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $errors->first('description') }}</strong>
+                  </span>
+              @endif
+            </div> 
           </div>
+
+          <fieldset id="home-visitation" class="p-3 mb-2 border-1" style="border-radius:4px;border:1px solid #ccc;display: none;">
+            <legend class="h5 py-1">Home Visitation</legend>
+
+            <div class="form-row mb-2">
+              <label for="address">Address*</label>
+              <input type="text" name="address" maxlength="255" value="{{old('address')}}" placeholder="address for home visit" id="address" class="form-default form-control{{ $errors->has('address') ? ' is-invalid' : '' }}">
+
+              @if ($errors->has('address'))
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $errors->first('address') }}</strong>
+                  </span>
+              @endif
+            </div>
+
+            <div class="form-row">
+              <label for="phone">Phone Contact*</label>
+              <input type="tel" name="phone" maxlength="255" value="{{old('phone')}}" placeholder="phone for home visit" id="phone" class="form-default form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}">
+
+              @if ($errors->has('phone'))
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $errors->first('phone') }}</strong>
+                  </span>
+              @endif
+            </div>
+          </fieldset>
 
           <div class="form-row">
-            <label for="phone">Phone Contact*</label>
-            <input type="tel" name="phone" maxlength="255" value="{{old('phone')}}" placeholder="phone for home visit" id="phone" class="form-default form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}">
-
-            @if ($errors->has('phone'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('phone') }}</strong>
-                </span>
-            @endif
+            <div class="col-md-12">
+              <button type="submit" class="btn btn-block btn-primary"><i class="fa fa-image"></i> Submit</button>
+            </div>
           </div>
-        </fieldset>
 
-        <div class="form-row">
-          <div class="col-md-12">
-            <button type="submit" class="btn btn-block btn-primary"><i class="fa fa-image"></i> Submit</button>
-          </div>
-        </div>
+        </form>
 
-      </form>
+      @endif
+
+    @else
+
+      <div class="col text-center font-weight-bold">
+        <div class="display-3"><i class="fa fa-ban red"></i></div>
+        <p class="alert alert-danger">
+          Not available for appointments at this time.
+        </p>
+      </div>
 
     @endif
   </div>     
