ea6dd9179ec813e6526473e58ef86bdf42f53c72
tonyfrenzy <tonyfrenzy@gmail.com>
2019-04-19 20:48:05 +0100

    Footer UI updated

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,9 +1,36 @@
 <footer id="main-footer-override" class="main-footer bg-dark">
     <div class="container">
-        <div class="footer-copyright  text-theme-blue">
+        <div class="container footer-copyright">
 
-            <strong>{{config('app.name')}} &copy; {{ date('Y') }}</strong>
-            
+            {{-- <div class="d-block mx-auto"> --}}
+            <ul class="list-unstyled mb-3 mx-auto">
+                <li class="nav-item text-center mb-2">
+                  <ul class="list-inline">
+                      <li class="nav-item mb-2 list-inline-item">
+                          <a class="nav-link" href="#">About</a>
+                      </li>
+                      <li class="nav-item mb-2 list-inline-item">
+                          <a class="nav-link" href="#">FAQs</a>
+                      </li>
+                      <li class="nav-item mb-2 list-inline-item">
+                          <a class="nav-link" href="#">Contact Us</a>
+                      </li>
+                  </ul>
+                </li>
+                {{-- 
+                <li class="nav-item text-center mb-2">
+                  <span>...</span>
+                </li> --}}
+            </ul>
+
+            <div class="text-center">
+                <span class="text-theme-blue">
+
+                    <strong>{{config('app.name')}} &copy; {{ date('Y') }}</strong>
+                  
+                </span>
+            </div>
+
         </div>
     </div>
 </footer>
